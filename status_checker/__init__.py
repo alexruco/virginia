88ba9f3db62b2faee84b3a7417b16a1e41810123
Alex Ruco <alex@ruco.pt>
2024-07-31 17:12:36 +0100

    added status_checker. before https_checker on init.py

--- status_checker/__init__.py
+++ status_checker/__init__.py
@@ -1,4 +1,4 @@
 # status_checker/__init__.py
 
 from status_checker.main import check_page_availability
-from https_checker import check_http2_status
+from status_checker.https_checker import check_http2_status
