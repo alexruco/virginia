1b0f3c2db6a96aa92c2f3723a43b3a0fe0616224
Alex Ruco <alex@ruco.pt>
2024-07-31 17:08:08 +0100

    fixed check_http2_status on init.py

--- status_checker/__init__.py
+++ status_checker/__init__.py
@@ -1,3 +1,4 @@
 # status_checker/__init__.py
 
-from status_checker.main import check_http2_status, check_page_availability
+from status_checker.main import check_page_availability
+from https_checker import check_http2_status
