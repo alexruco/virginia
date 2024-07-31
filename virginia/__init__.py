a89d2870cb5a334baf3d3ae84d3676b750329bb4
Alex Ruco <alex@ruco.pt>
2024-07-31 19:58:06 +0100

    fixed a typo

---
+++ virginia/__init__.py
@@ -0,0 +1,4 @@
+# status_checker/__init__.py
+
+from virginia.main import check_page_availability
+from virginia.https_checker import check_http2_status
--- viriginia/__init__.py
+++
@@ -1,4 +0,0 @@
-# status_checker/__init__.py
-
-from viriginia.main import check_page_availability
-from viriginia.https_checker import check_http2_status
