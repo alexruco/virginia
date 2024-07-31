50c3a442c5f9647ca73757b29189d206a3e58030
Alex Ruco <alex@ruco.pt>
2024-07-31 16:53:38 +0100

    added setup.py

---
+++ status_checker/status.py
@@ -0,0 +1,33 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="virginia",
+    version="0.1.0",
+    author="Alex Ruco",
+    author_email="alex@ruco.pt",
+    description="A Python library for webpage status checking",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexruco/virginia",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "httpx[http2]",
+    ],
+    entry_points={
+        "console_scripts": [
+            "check_page=status_checker.main:check_page_availability",
+            "check_http2=status_checker.https_checker:check_http2_status",
+        ],
+    },
+)
